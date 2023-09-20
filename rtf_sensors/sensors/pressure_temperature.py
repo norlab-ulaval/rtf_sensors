5e0dc7f2e05a401e0c216ed7d808f4a2209a3ecf
sphere <matej.boxan@gmail.com>
2023-09-20 15:53:59 -0400

    Publish temperature and pressure instead of altitude

--- rtf_sensors/sensors/pressure_temperature.py
+++ rtf_sensors/sensors/pressure_temperature.py
@@ -6,8 +6,8 @@
 ##############################################
 import rclpy
 from rclpy.node import Node
-# from sensor_msgs.msg import Temperature, FluidPressure
-from geometry_msgs.msg import PointStamped
+from sensor_msgs.msg import Temperature, FluidPressure
+#from geometry_msgs.msg import PointStamped
 import board
 import busio
 
@@ -40,40 +40,40 @@ class rtf_PT(Node):
 
         self.timer = self.create_timer(1.0, self.callback)
 
-        # self.pub_temp = self.create_publisher(Temperature, 'temperature', 10)
-        # self.pub_pressure = self.create_publisher(FluidPressure, 'pressure', 10)
-        self.pub_altitude = self.create_publisher(PointStamped, 'altitude', 10)
+        self.pub_temp = self.create_publisher(Temperature, 'temperature', 10)
+        self.pub_pressure = self.create_publisher(FluidPressure, 'pressure', 10)
+        #self.pub_altitude = self.create_publisher(PointStamped, 'altitude', 10)
 
-        self.frame_id = self.declare_parameter('frame_id', "base_altitude_link").value
+        self.frame_id = self.declare_parameter('frame_id', "dps310").value
 
-        # self.temp_msg = Temperature()
-        # self.temp_msg.header.frame_id = frame_id
-        # self.temp_msg.variance = 0.01
-        #
-        # self.pressure_msg = FluidPressure()
-        # self.pressure_msg.header.frame_id = frame_id
-        # self.pressure_msg.variance = 0.01
+        self.temp_msg = Temperature()
+        self.temp_msg.header.frame_id = self.frame_id
+        self.temp_msg.variance = 0.01
+        
+        self.pressure_msg = FluidPressure()
+        self.pressure_msg.header.frame_id = self.frame_id
+        self.pressure_msg.variance = 0.01
 
     def callback(self):
         stamp = self.get_clock().now().to_msg()
-        # t = self.sensor.temperature
-        # self.temp_msg.header.stamp = stamp
-        # self.temp_msg.temperature = t # C
-        # self.pub_temp.publish(self.temp_msg)
-        #
+        t = self.sensor.temperature
+        self.temp_msg.header.stamp = stamp
+        self.temp_msg.temperature = t # C
+        self.pub_temp.publish(self.temp_msg)
+        
         p = self.sensor.pressure
         p0 = 1013.25 # hPa
-        # self.pressure_msg.header.stamp = stamp
-        # self.pressure_msg.fluid_pressure = p*100 # Pa
-        # self.pub_pressure.publish(self.pressure_msg)
+        self.pressure_msg.header.stamp = stamp
+        self.pressure_msg.fluid_pressure = p*100 # Pa
+        self.pub_pressure.publish(self.pressure_msg)
 
-        msg = PointStamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = self.frame_id
-        msg.point.x = 0.0
-        msg.point.y = 0.0
-        msg.point.z = 44330. * (1. - (p/p0)**(1./5.255) )
-        self.pub_altitude.publish(msg)
+        #msg = PointStamped()
+        #msg.header.stamp = self.get_clock().now().to_msg()
+        #msg.header.frame_id = self.frame_id
+        #msg.point.x = 0.0
+        #msg.point.y = 0.0
+        #msg.point.z = 44330. * (1. - (p/p0)**(1./5.255) )
+        #self.pub_altitude.publish(msg)
 
 
 class rtf_dps310(rtf_PT):
