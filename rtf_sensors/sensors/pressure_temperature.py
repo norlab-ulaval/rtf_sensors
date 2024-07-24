4d6eb8aa0724f80fcde0e4ef13142661a69824b6
WilliamDubois <william.dubois@usherbrooke.ca>
2024-07-24 15:41:01 -0400

    modified to publish only one message containing all the information (no duplication).

--- rtf_sensors/sensors/pressure_temperature.py
+++ rtf_sensors/sensors/pressure_temperature.py
@@ -6,7 +6,7 @@
 ##############################################
 import rclpy
 from rclpy.node import Node
-from rtf_sensors_msgs.msg import CustomTemperature, CustomPressure
+from rtf_sensors_msgs.msg import CustomPressureTemperature
 #from geometry_msgs.msg import PointStamped
 import board
 import busio
@@ -41,55 +41,52 @@ class rtf_PT(Node):
 
         self.timer = self.create_timer(rate, self.callback)
 
-        self.pub_temp = self.create_publisher(CustomTemperature, 'temperature', 10)
-        self.pub_pressure = self.create_publisher(CustomPressure, 'pressure', 10)
+        self.pub_pressure_temp = self.create_publisher(CustomPressureTemperature, 'data', 10)
         #self.pub_altitude = self.create_publisher(PointStamped, 'altitude', 10)
 
         self.frame_id = self.declare_parameter('frame_id', "dps310").value
         self.i2c_address = self.declare_parameter('i2c_address', "0x77").value
-
-        self.temp_msg = CustomTemperature()
-        self.temp_msg.header.frame_id = self.frame_id
         
-        self.pressure_msg = CustomPressure()
-        self.pressure_msg.header.frame_id = self.frame_id
+        self.pressure_temp_msg = CustomPressureTemperature()
+        self.pressure_temp_msg.header.frame_id = self.frame_id
 
     def callback(self):
         stamp = self.get_clock().now().to_msg()
-        self.temp_msg.header.stamp = stamp
-        self.temp_msg.c0 = float(self.sensor._c0)
-        self.temp_msg.c1 = float(self.sensor._c1)
-        raw_temp = self.sensor._raw_temperature
-        # raw temperature value is a two's complement so we need to deal with that.
-        # 24 is the number of bits for the raw measurement bit shift when necessary.
-        if raw_temp & (1 << (24 - 1)):
-            raw_temp -= 1 << 24
-        self.temp_msg.raw_temperature = float(raw_temp)
-        self.temp_msg.scale_temperature = float(self.sensor._temp_scale)
-        t = self.sensor.temperature
-        self.temp_msg.temperature = t # C
-        self.pub_temp.publish(self.temp_msg)
+        self.pressure_temp_msg.header.stamp = stamp
         
-        self.pressure_msg.header.stamp = stamp
-        self.pressure_msg.c00 = float(self.sensor._c00)
-        self.pressure_msg.c01 = float(self.sensor._c01)
-        self.pressure_msg.c10 = float(self.sensor._c10)
-        self.pressure_msg.c11 = float(self.sensor._c11)
-        self.pressure_msg.c20 = float(self.sensor._c20)
-        self.pressure_msg.c21 = float(self.sensor._c21)
-        self.pressure_msg.c30 = float(self.sensor._c30)
+        self.pressure_temp_msg.c0 = float(self.sensor._c0)
+        self.pressure_temp_msg.c1 = float(self.sensor._c1)
+        self.pressure_temp_msg.c00 = float(self.sensor._c00)
+        self.pressure_temp_msg.c01 = float(self.sensor._c01)
+        self.pressure_temp_msg.c10 = float(self.sensor._c10)
+        self.pressure_temp_msg.c11 = float(self.sensor._c11)
+        self.pressure_temp_msg.c20 = float(self.sensor._c20)
+        self.pressure_temp_msg.c21 = float(self.sensor._c21)
+        self.pressure_temp_msg.c30 = float(self.sensor._c30)
+        
         raw_pressure = self.sensor._raw_pressure
         # raw pressure value is a two's complement so we need to deal with that.
         # 24 is the number of bits for the raw measurement bit shift when necessary.
         if raw_pressure & (1 << (24 - 1)):
             raw_pressure -= 1 << 24
-        self.pressure_msg.raw_pressure = float(raw_pressure)
-        self.pressure_msg.scale_pressure = float(self.sensor._pressure_scale)
-        self.pressure_msg.raw_temperature = float(raw_temp)
-        self.pressure_msg.scale_temperature = float(self.sensor._temp_scale)        
+        self.pressure_temp_msg.raw_pressure = float(raw_pressure)
+        self.pressure_temp_msg.scale_pressure = float(self.sensor._pressure_scale)
+        
+        raw_temp = self.sensor._raw_temperature
+        # raw temperature value is a two's complement so we need to deal with that.
+        # 24 is the number of bits for the raw measurement bit shift when necessary.
+        if raw_temp & (1 << (24 - 1)):
+            raw_temp -= 1 << 24
+        self.pressure_temp_msg.raw_temperature = float(raw_temp)
+        self.pressure_temp_msg.scale_temperature = float(self.sensor._temp_scale)        
+        
         p = self.sensor.pressure
-        self.pressure_msg.pressure = p*100 # Pa
-        self.pub_pressure.publish(self.pressure_msg)
+        self.pressure_temp_msg.pressure = p*100 # Pa
+        
+        t = self.sensor.temperature
+        self.pressure_temp_msg.temperature = t # C
+        
+        self.pub_pressure_temp.publish(self.pressure_temp_msg)
 
         #msg = PointStamped()
         #msg.header.stamp = self.get_clock().now().to_msg()
